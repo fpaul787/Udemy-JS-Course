5284505175d54ecdc5b66e7a4a8c4623aec4ba37
Frantz <fpaul787@gmail.com>
2019-09-02 22:03:07 -0400

    Made API request with axios and added dependency

--- Section 9/src/js/index.js
+++ Section 9/src/js/index.js
@@ -23,3 +23,35 @@
 // console.log(`Using imported function! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3, 5)}. ${str}`);
 
 
+
+////////////////////////////////////////////
+// Lecture: Making our first API
+
+import axios from 'axios';
+
+async function getResults(query){
+
+    
+    //older browsers might not recognize fetch
+    // using axios to do ajax call
+
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = '6bd13bc05e1ae4ec00fabf0a3c299d54';
+
+    try{
+         //REMEMBER, we handle promises using async, await
+    const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+    const recipes = res.data.recipes;
+    console.log(recipes);
+    }catch(error){
+        alert(error);
+    }
+
+    // error handling is easier in axios than in fetch
+   
+}
+
+getResults('pizza');
+getResults('sandwich')
+
+
